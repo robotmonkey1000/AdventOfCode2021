cffea910097a99bef2c8b751711b3e37c4e69512
Robotmonkey1000 <robotmonkey000@gmail.com>
2021-12-01 10:18:09 -0500

    Add original files.

---
+++ AdventOfCode2021/AdventOfCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    class AdventOfCode
+    {
+        static void Main(string[] args)
+        {
+            Day1 day1 = new Day1();
+            day1.Run();
+
+
+            Console.WriteLine("\nPress Enter plz");
+            Console.ReadLine();
+        }
+
+    }
+}
