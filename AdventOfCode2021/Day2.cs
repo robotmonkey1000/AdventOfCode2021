d9313995cf66014f0d57966e12668222c22e5cb6
robotmonkey1000 <robotmonkey000@gmail.com>
2021-12-02 13:36:03 -0500

    Day2 Completed (#1)
    * Day 2 Completed
    
    * Updated Readme

---
+++ AdventOfCode2021/Day2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+    class Day2
+    {
+        public void Run()
+        {
+            Console.WriteLine("~~Part 1~~");
+            Part1();
+
+            Console.WriteLine("~~Part 2~~");
+            Part2();
+        }
+        public void Part1()
+        {
+            int depth = 0;
+            int horz = 0;
+
+
+            string[] lines = System.IO.File.ReadAllLines("./Day2Data.txt");
+            foreach(string line in lines)
+            {
+                string[] actions = line.Split(' ');
+                if(actions[0].Equals("forward"))
+                {
+                    horz += int.Parse(actions[1]);
+                }
+                else if(actions[0].Equals("up"))
+                {
+                    depth -= int.Parse(actions[1]);
+                }
+                else if (actions[0].Equals("down"))
+                {
+                    depth += int.Parse(actions[1]);
+                }
+            }
+
+            Console.WriteLine($"Depth: {depth}, Horz: {horz}, Final: {depth * horz}");
+
+        }
+
+        public void Part2()
+        {
+            int depth = 0;
+            int horz = 0;
+            int aim = 0;
+
+
+            string[] lines = System.IO.File.ReadAllLines("./Day2Data.txt");
+            foreach (string line in lines)
+            {
+                string[] actions = line.Split(' ');
+                if (actions[0].Equals("forward"))
+                {
+                    horz += int.Parse(actions[1]);
+                    depth += aim * int.Parse(actions[1]);
+                }
+                else if (actions[0].Equals("up"))
+                {
+                    aim -= int.Parse(actions[1]);
+                }
+                else if (actions[0].Equals("down"))
+                {
+                    aim += int.Parse(actions[1]);
+                }
+            }
+
+            Console.WriteLine($"Depth: {depth}, Horz: {horz}, Final: {depth * horz}");
+        }
+    }
+}
